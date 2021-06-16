2035ff1a283a75f446a1ed771876336706b38f33
Julia Taranenko <julia.taranenko.25@gmail.com>
2021-06-16 22:39:52 +0300

    Initial commit

--- src/App.vue
+++ src/App.vue
@@ -1,29 +1,73 @@
 <template>
-  <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js + TypeScript App"/>
+  <div id="app" :data-dark="useDark">
+    <autocomplete
+            :options="cities"
+            v-model="city"
+            @change="onCitySelect"
+    />
+    <div class="city">
+      {{city.name}}
+    </div>
+    <label>
+      Dark mode
+      <input
+              type="checkbox"
+              v-model="useDark"
+      />
+    </label>
   </div>
 </template>
 
 <script lang="ts">
 import { Component, Vue } from 'vue-property-decorator';
-import HelloWorld from './components/HelloWorld.vue';
+import Autocomplete from "@/components/Autocomplete.vue";
+import {IOption} from "@/helpers/ioption";
 
 @Component({
   components: {
-    HelloWorld,
-  },
+    Autocomplete
+  }
 })
-export default class App extends Vue {}
+
+export default class App extends Vue {
+  private city: IOption = {
+    name: 'Riga',
+    id: 90
+  };
+
+  private cities: Array<IOption> = [
+    {
+      name: 'Riga',
+      id: 90
+    },
+    {
+      name: 'Daugavpils',
+      id: 78
+    },
+  ];
+
+  private useDark = false;
+
+  onCitySelect(option: IOption): void {
+    this.city = option;
+  }
+}
 </script>
 
 <style lang="scss">
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
-}
+  @import "styles/variables.css";
+
+  #app {
+    font-family: Avenir, Helvetica, Arial, sans-serif;
+    -webkit-font-smoothing: antialiased;
+    -moz-osx-font-smoothing: grayscale;
+    text-align: center;
+    margin-top: 60px;
+    background-color: var(--bg-color);
+    color: var(--text-color);
+  }
+
+  .city {
+    color: var(--text-color);
+  }
 </style>
---
+++ src/components/Autocomplete.vue
@@ -0,0 +1,115 @@
+<template>
+    <div class="autocomplete">
+        <input
+                class="autocomplete__input"
+                type="text"
+                :value="searchTerm"
+                @input="onFilter($event.target.value)"
+        >
+        <button
+                class="autocomplete__button"
+                @click="onClear"
+        >
+            X
+        </button>
+        <ul class="autocomplete__dropdown">
+            <li
+                    v-for="option in filteredOptions"
+                    :key="option.id"
+                    @click="onSelect(option)"
+            >
+                {{option.name}}
+            </li>
+        </ul>
+    </div>
+</template>
+
+<script lang="ts">
+    import { Component, Prop, Vue } from 'vue-property-decorator';
+    import {IOption} from "@/helpers/ioption";
+
+    @Component
+    export default class Autocomplete extends Vue {
+        private searchTerm = '';
+
+        @Prop()
+        private options!: Array<IOption>;
+
+        get filteredOptions(): Array<IOption> {
+            let searchTermLowerCase = this.searchTerm.toLowerCase();
+            return searchTermLowerCase.length === 0 ? this.options : this.options.filter(option => {
+                return option.name.toLowerCase().includes(searchTermLowerCase);
+            });
+        }
+
+        onSelect(option: IOption): void {
+            this.searchTerm = option.name;
+            this.$emit("change", option);
+        }
+
+        onFilter(searchTerm: string): void {
+            this.searchTerm = searchTerm;
+        }
+
+        onClear(): void {
+            this.searchTerm = '';
+        }
+    }
+</script>
+
+<style scoped lang="scss">
+    .autocomplete {
+        position: relative;
+        display: inline-block;
+        border: 1px solid var(--border-color);
+        background-color: var(--input-color);
+
+        &:focus-within {
+            .autocomplete__dropdown {
+                display: block;
+            }
+        }
+    }
+
+    .autocomplete__input {
+        border: none;
+        outline: none;
+        background-color: transparent;
+        color: var(--text-color);
+    }
+
+
+    .autocomplete__button {
+        background-color: transparent;
+        color: var(--text-color);
+        border: none;
+        outline: none;
+        cursor: pointer;
+    }
+
+    .autocomplete__dropdown {
+        position: absolute;
+        z-index: 1;
+        right: auto;
+        left: auto;
+        width: 100%;
+        display: none;
+        list-style: none;
+        text-align: start;
+        padding-inline-start: 0;
+        background-color: var(--bg-color);
+        border: 1px solid var(--border-color);
+        margin: 0;
+        max-height: 200px;
+
+        li {
+            padding: 2px 4px;
+            width: 100%;
+            cursor: pointer;
+
+            &:hover {
+                background-color: beige;
+            }
+        }
+    }
+</style>
