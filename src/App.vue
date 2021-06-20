33d84ee3d1ff1259be5e2b5246b61c48d57be9fe
Julia Taranenko <julia.taranenko.25@gmail.com>
2021-06-20 18:32:41 +0300

    fix: fix autocomplete getting invisible on mobile keyboard open

--- src/App.vue
+++ src/App.vue
@@ -44,8 +44,7 @@ export default class App extends Vue {
     left: 0;
     right: 0;
     bottom: 0;
-    display: flex;
-    justify-content: center;
+    background: var(--bg-color);
   }
 
   #app {
@@ -57,6 +56,5 @@ export default class App extends Vue {
     justify-content: center;
     align-items: center;
     flex-direction: column;
-    background: var(--bg-color);
   }
 </style>
